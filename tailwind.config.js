cb478b3896f20637143c2219bdd51142adab5a56
Rsmile310 <78564494+Rsmile310@users.noreply.github.com>
2022-03-27 05:36:33 +0900

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,45 @@ tailwind.config = {
     backgroundImage:{
       'hero':"url(assets/images/hero.png)",
       'hydragun':"url(assets/images/hydragun.png)",
-      'linear-grey' : 'linear-gradient(90deg, #E1E1E1 46.94%, rgba(225, 225, 225, 0) 63.47%)'
+      'linear-grey' : 'linear-gradient(90deg, #E1E1E1 46.94%, rgba(225, 225, 225, 0) 63.47%)',
+      'linear-opacity': 'linear-gradient(90deg, #E1E1E1 52.94%, rgba(225, 225, 225, 0) 64.49%)',
+      'linear-gray': 'linear-gradient(90deg, #E1E1E1 52.94%,#E1E1E1  64.49%)'
     },
     maxWidth: {
       '280': '280px',
+      '300': '300px',
       '308': '308px',
       '348': '348px',
+      '351': '351px',
+      '368': '368px',
+      '403': '403',
+      '444': '444px',
+      '453': '453px',
+      '497': '497px',
+      '500': '470px',
+      '555': '555px',
+      '600': '600px',
       '626': '626px',
       '680': '680px',
       '552': '552px',
+      '673': '673px',
       '779': '779px',
       '1054': '1054px',
       '1232': '1232px',
       '1384': '1384px'
     },
+    width:{
+      '1900':'1900px',
+      '1286':'1286px',
+      '1054': '1054px',
+      '779': '779px',
+      '102':'102px',
+      '368':'368px'
+    },
     height:{
-      '308': '308px'
+      '308': '308px',
+      '192':'192px',
+      '672':'672px'
     },
     spacing:{
       'middle' : '40%',
